828fecc63e4781e8e438357fe32c67e161054567
limbaba1120 <kwlim1120ss@gmail.com>
2023-10-13 18:47:20 +0900

    jpa observation

---
+++ src/main/java/study/datajpa/entity/Item.java
@@ -0,0 +1,16 @@
+package study.datajpa.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+
